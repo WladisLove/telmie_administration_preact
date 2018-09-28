4367dbc9cba3f4447ddbbe279156b28fc22a2575
WladisLove <klim12121997@yandex.by>
2018-09-28 18:09:07 +0300

    add antd UI-components; add test table to active users route

--- src/routes/users-active/index.js
+++ src/routes/users-active/index.js
@@ -1,10 +1,170 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
+import Card from '../../components/card'
+import { Table, Checkbox, Button } from 'antd';
+import 'antd/dist/antd.css';
+import style from './style'
 
-const ActiveUsers = () => (
-	<div class='route-content'>
-		<h1>ActiveUsers</h1>
-		<p>This is the ActiveUsers component.</p>
-	</div>
-);
 
-export default ActiveUsers;
+const columns = [{
+  title: 'Id',
+  dataIndex: 'id',
+  defaultSortOrder: 'ascend',
+  sorter: (a, b) => a.id - b.id,
+},{
+  title: 'First name',
+  dataIndex: 'firstName',
+  sorter: (a, b) => a.firstName.length - b.firstName.length,
+},{
+  title: 'Last name',
+  dataIndex: 'lastName',
+  sorter: (a, b) => a.lastName.length - b.lastName.length,
+}, {
+  title: 'Email',
+  dataIndex: 'email',
+  sorter: (a, b) => a.email.length - b.email.length,
+},{
+	title: 'Status',
+	dataIndex: 'status',
+	sorter: (a, b) => a.status.length - b.status.length,
+	onFilter: (value, record) => console.log(value, record),
+},{
+  title: 'Last active',
+  dataIndex: 'lastActive',
+  sorter: (a, b) => a.lastActive.length - b.lastActive.length,
+},{
+  title: 'Credit card',
+  dataIndex: 'creditCard',
+  sorter: (a, b) => a.creditCard.length - b.creditCard.length,
+},{
+  title: 'Registration date',
+  dataIndex: 'registrationDate',
+  sorter: (a, b) => a.registrationDate.length - b.registrationDate.length,
+},{
+  title: 'Bank account',
+  dataIndex: 'bankAccount',
+  sorter: (a, b) => a.bankAccount.length - b.bankAccount.length,
+},{
+  title: 'Total earning',
+  dataIndex: 'totalEarning',
+  sorter: (a, b) => a.totalEarning - b.totalEarning,
+},];
+
+function onChange(pagination, filters, sorter) {
+  console.log('params', pagination, filters, sorter);
+}
+
+const statusArr = [{
+	name: 'Registered',
+}, {
+	name: 'Suspended',
+}, {
+	name: 'Started Pro Appl',
+}, {
+	name: 'Pending Approval',
+}, {
+	name: 'Approved as Pro',
+}, {
+	name: 'Suspended as Pro',
+}];
+
+const getDefaultStatuses = () => statusArr.map(el => el.name);
+
+
+export default class ActiveUsers extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			statusFilter: getDefaultStatuses() || [],
+			isFiltered: false,
+			filteredData: [],
+			data: [{
+				id: '11245',
+				firstName: 'John',
+				lastName: 'Brown',
+				email: '',
+				status: 'Started Pro Appl',
+				lastActive: '',
+				creditCard: 'yes',
+				registrationDate: '',
+				bankAccount: 'no',
+				totalEarning: 22,
+			}, {
+				id: '2',
+				firstName: 'Jim',
+				lastName: 'Green',
+				email: '',
+				status: 'Approved as Pro',
+				lastActive: '',
+				creditCard: 'no',
+				registrationDate: '',
+				bankAccount: 'yes',
+				totalEarning: 2,
+			}, {
+				id: '3',
+				firstName: 'Joe',
+				lastName: 'Black',
+				email: 'klimen1997@gmail/com',
+				status: 'Approved as Pro',
+				lastActive: '',
+				creditCard: 'yes',
+				registrationDate: '',
+				bankAccount: 'yes',
+				totalEarning: 1,
+			}, {
+				id: '4',
+				firstName: 'Jim',
+				lastName: 'Red',
+				email: '',
+				status: 'Registered',
+				lastActive: '',
+				creditCard: 'no',
+				registrationDate: '',
+				bankAccount: 'yes',
+				totalEarning: 0,
+			}]
+		}
+	}
+
+	onChange = (checkedValues) => {
+		this.setState({statusFilter: [...checkedValues]});
+	}
+
+	onFilter = () => {
+		const {data = [], statusFilter} = this.state;
+		if (statusFilter.length === statusArr.length){
+			this.setState({ filteredData: [], isFiltered: false });
+		} else {
+			let newData = data.filter(el => statusFilter.indexOf(el.status) != -1 );
+			this.setState({ filteredData: newData, isFiltered: true });
+		}
+		
+	}
+
+	render(){
+		const {statusFilter, data, isFiltered, filteredData} = this.state;
+
+		const dataSource = isFiltered ? filteredData : data;
+		return (
+			<Card cardClass='route-content'>
+				<h1>Active Users</h1>
+				<div class={style.filterGroup}>
+					<div class={style.filterGroupLabel}> Status filter: </div>
+					
+					<Checkbox.Group onChange={this.onChange} value={statusFilter}	>
+						{statusArr.map(({name}) => (<Checkbox className={statusFilter.indexOf(name) != -1 && style.checked} 
+																		key={name} value={name}>{name}</Checkbox>))}
+					</Checkbox.Group>
+					
+					<Button size='small' onClick={this.onFilter} className={style.filterBtn}>Filter</Button>
+				</div>
+					
+				<Table columns={columns} 
+					rowKey={(record) => record.id} 
+					dataSource={dataSource} 
+					onChange={onChange} />
+			</Card>
+		)
+	}
+	
+};
