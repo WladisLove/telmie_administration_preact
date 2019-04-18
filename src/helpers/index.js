147e970ab0aa2584ab747021563a1ed10ce6a960
WladisLove <klim12121997@yandex.by>
2019-04-18 21:22:26 +0300

    add date handler for daylight saving time

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,13 +1,29 @@
 export function convertDate(date) {
+    Date.prototype.isDstObserved && setDstObserved();
+
     let _d = null;
-    const d = (typeof date === 'string' || date instanceof String) ? (
+    let d = (typeof date === 'string' || date instanceof String) ? (
         _d = date.split(/[^0-9]/),
         new Date (_d[0], _d[1]-1, _d[2], _d[3], _d[4], _d[5])
     ) : new Date(date);
     
     return (isNaN(d.getTime()) || !date) ?
         'Invalid date'
-        : `${d.getDate().toString().padStart(2, "0")}/${(d.getMonth() + 1).toString().padStart(2, "0")}/${d.getFullYear()} ${d.getHours().toString().padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}`;
+        : (
+            d.isDstObserved && d.setHours(d.getHours() + 1),
+            `${d.getDate().toString().padStart(2, "0")}/${(d.getMonth() + 1).toString().padStart(2, "0")}/${d.getFullYear()} ${d.getHours().toString().padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}`
+        );
+}
+
+function setDstObserved(){
+    Date.prototype.stdTimezoneOffset = function () {
+        let jan = new Date(this.getFullYear(), 0, 1);
+        let jul = new Date(this.getFullYear(), 6, 1);
+        return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+    }
+    Date.prototype.isDstObserved = function () {
+        return this.getTimezoneOffset() < this.stdTimezoneOffset();
+    }
 }
 
 function converVersionToNum(version){
